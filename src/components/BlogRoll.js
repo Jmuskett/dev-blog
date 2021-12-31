b237823b4d75a6b79b0bac830bc16b57180be426
John <hi@johnmuskett.com>
2021-12-31 15:13:50 +0000

    implement horizontal scroll on blog posts

--- src/components/BlogRoll.js
+++ src/components/BlogRoll.js
@@ -3,51 +3,55 @@ import PropTypes from "prop-types";
 import { Link, graphql, StaticQuery } from "gatsby";
 import PreviewCompatibleImage from "./PreviewCompatibleImage";
 import { GatsbyImage } from "gatsby-plugin-image";
+import { Parallax, ParallaxLayer } from "@react-spring/parallax";
 
 class BlogRollTemplate extends React.Component {
   render() {
     const { data } = this.props;
     const { edges: posts } = data.allMarkdownRemark;
-
+    const numberOfPosts = posts ? posts.length : 0;
     return (
-      <div className="px-11 flex flex-initial  flex-col md:flex-row">
-        {posts &&
-          posts.map(({ node: post }) => {
-            return (
-              <div key={post.id}>
-                <Link
-                  className="text-4xl text-white hover:text-white my-6"
-                  to={post.fields.slug}
-                >
-                  <article className="text-center  px-11 py-11 my-11 border-solid  rounded-3xl shadow-xl  hover:cursor-pointer md:mx-6   ">
-                    <header>
-                      {post.frontmatter.featuredimage ? (
+      <div className="px-11 py-48 flex min-h-screen items-stretch justify-center">
+        <Parallax horizontal pages={numberOfPosts}>
+          {posts &&
+            posts.map(({ node: post }, idx) => {
+              return (
+                <ParallaxLayer offset={idx} key={post.id}>
+                  <div className="grow-0 shrink-0 ">
+                    <Link
+                      className="text-4xl text-white hover:text-white my-6"
+                      to={post.fields.slug}
+                    >
+                      <article className="text-center  px-11 py-11 my-11 border-solid  rounded-3xl shadow-xl  hover:cursor-pointer md:mx-6   ">
+                        <header>
+                          {post.frontmatter.featuredimage ? (
+                            <div>
+                              <GatsbyImage
+                                className="mb-6  rounded-lg  hover:ring hover:ring-white hover:ring-offset-4 hover:ring-offset-red-500 "
+                                image={
+                                  post.frontmatter.featuredimage.childImageSharp
+                                    .gatsbyImageData
+                                }
+                              />
+                            </div>
+                          ) : null}
+                          <p>{post.frontmatter.title}</p>
+                        </header>
                         <div>
-                          <GatsbyImage
-                            className="mb-6  rounded-lg  hover:ring hover:ring-white hover:ring-offset-4 hover:ring-offset-red-500 "
-                            style={{ width: "100%" }}
-                            image={
-                              post.frontmatter.featuredimage.childImageSharp
-                                .gatsbyImageData
-                            }
-                          />
+                          <span>{post.frontmatter.date}</span>
                         </div>
-                      ) : null}
-                      <p>{post.frontmatter.title}</p>
-                    </header>
-                    <div>
-                      <span>{post.frontmatter.date}</span>
-                    </div>
-                    {/* <p>
+                        {/* <p>
                   {post.excerpt}
                   <br />
                   <br />
                 </p> */}
-                  </article>
-                </Link>
-              </div>
-            );
-          })}
+                      </article>
+                    </Link>
+                  </div>
+                </ParallaxLayer>
+              );
+            })}
+        </Parallax>
       </div>
     );
   }
